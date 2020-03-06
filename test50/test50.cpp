33fb3f2211e1f0ff69afad22ce73aa7865ff1c94
Bob <bob@bob.com>
2020-03-06 13:37:03 -0500

    Ajout d'un 3e message

--- test50/test50.cpp
+++ test50/test50.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
 	std::cout << "Salut la Terre";
+	std::cout << "Hello ";
 }
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
